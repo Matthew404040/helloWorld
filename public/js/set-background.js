a028bc766f42b7848307b6e4558913d7a03fe527
Matthew404040 <31577756+Matthew404040@users.noreply.github.com>
2018-08-01 12:38:41 -0400

    Update set-background.js

--- public/js/set-background.js
+++ public/js/set-background.js
@@ -1,5 +1,16 @@
 var idx = Math.floor((new Date().getHours()));
 var body = document.getElementsByTagName("body")[0];
 body.className = "heaven-" + idx;
-var d = new Date();
-console.log(d.getSeconds());
+
+const server = require('http').createServer();
+
+const io = require('socket.io')(server, {
+  path: '/test',
+  serveClient: false,
+  // below are engine.IO options
+  pingInterval: 10000,
+  pingTimeout: 5000,
+  cookie: false
+});
+
+server.listen(3000);
