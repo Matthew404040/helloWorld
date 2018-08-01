cbda03c228a04abd4beee328748ce15d7602021f
Matthew404040 <31577756+Matthew404040@users.noreply.github.com>
2018-08-01 12:28:19 -0400

    Update set-background.js

--- public/js/set-background.js
+++ public/js/set-background.js
@@ -1,5 +1,5 @@
 var idx = Math.floor((new Date().getHours()));
 var body = document.getElementsByTagName("body")[0];
 body.className = "heaven-" + idx;
-console.log('changes made');
-console.log('more changes made');
+var d = new Date();
+console.log(d.getSeconds());
